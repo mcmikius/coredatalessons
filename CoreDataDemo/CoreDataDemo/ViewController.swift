80a2b31bca85bad5d0acad5ed11059ee567c14b9
Michail Bondarenko <mcmikius15@gmail.com>
2019-02-06 13:20:25 +0200

    Initial commit

---
+++ CoreDataDemo/CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Michail Bondarenko on 2/6/19.
+//  Copyright © 2019 Michail Bondarenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
